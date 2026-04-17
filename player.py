0e98eefa6579104129f826174adadb320e6862c6
PranshuDesai <b24cs1057@iitj.ac.in>
2026-04-17 11:27:27 +0530

    Delete player.py

--- player.py
+++
@@ -1,9 +0,0 @@
-class Player:
-
-    def __init__(self):
-        self.name = "Player1"
-        self.score = 0
-
-    def makeMove(self):
-        move = input("\nPlayer Move: ")
-        return move
